3407c72bf78a9fabe67a040db7f7e8ab4cedf31a
Phan Tan Loc <phantanloc1988@gmail.com>
2021-06-04 22:12:19 +0700

    template customer + admin

---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace PlasticCompany.Models
+{
+    public partial class Contact
+    {
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/ProductCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace PlasticCompany.Models
+{
+    public partial class ProductCategory
+    {
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
